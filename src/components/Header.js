7c06e1be9ffdaa4daf23824108eed81eb16a3738
J. Ryan Williams <j_ryan_williams@me.com>
2017-11-20 02:20:56 -0700

    Added Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,15 @@ class Header extends Component {
         </div>
         <div className="Header-List">
           <ul>
-            <li className="">About</li>
-            <li>Contact</li>
-            <li>Blog</li>
+            <li className="List" id="About">About</li>
+            <li className="List" id="Contact">Contact</li>
+            <li className="List" id="Blog">Blog</li>
           </ul>
+        </div>
+        <div className="Social-Container">
+          <p>twitter</p>
+          <p>facebook</p>
+          <p>instagram</p>
         </div>
       </div>
     )
